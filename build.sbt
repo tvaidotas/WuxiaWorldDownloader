97d1113740dcc1ed6039e17fbb14e1d63d2032f1
admin <tadas.vaidotas@gmail.com>
2018-11-04 21:43:26 +0000

    manual work needed at this point

--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ scalaVersion := "2.12.7"
 
 libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "2.35.0"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5"
+libraryDependencies += "org.scala-lang.modules" %% "scala-swing" % "2.0.3"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,19 @@
+import java.io._
 import org.openqa.selenium.{By, WebDriver}
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.scalatest.selenium.WebBrowser
 
-object Main extends App with WebBrowser {
-
+object Main extends App with WebBrowser  {
   implicit val webDriver: WebDriver = new HtmlUnitDriver
-
-  val host = "https://www.wuxiaworld.com/"
-  val novel = "/novel/emperors-domination/emperor-chapter-" //637
-
-  val page = webDriver.get(host + novel + 637)
-
-  println(webDriver.findElement(By.className("fr-view")).getText)
-
+  val pw = new PrintWriter(new File("hello.txt" ))
+  for(i <- 637 to 638) {
+    webDriver.get("https://www.wuxiaworld.com/novel/emperors-domination/emperor-chapter-" + i)
+    Thread.sleep(400)
+    val content = webDriver.findElements(By.tagName("p"))
+    content.forEach(e => {
+      pw.write(e.getText + '\n')
+      pw.println()
+    })
+  }
+  pw.close
 }
